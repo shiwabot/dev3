aab59a0ddafdcc5f4d057f3e6fd87e9c542aca5f
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-07-15 12:27:16 +0530

    Update gcast.py

--- DaisyXMusic/modules/gcast.py
+++ DaisyXMusic/modules/gcast.py
@@ -1,45 +1,36 @@
-# Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+import asyncio
 
 from pyrogram import Client
 from pyrogram import filters
+from pyrogram.types import Dialog
+from pyrogram.types import Chat
+from pyrogram.types import Message
 from pyrogram.errors import UserAlreadyParticipant
-import asyncio
+
+from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 from DaisyXMusic.config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+@Client.on_message(filters.command(["broadcast"]))
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Starting Gcast")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Starting a broadcast...`")
         if not message.reply_to_message:
-            await lol.edit("Reply to any text message to gcast sir")
+            await wtf.edit("Please Reply to a Message to broadcast!")
             return
-        msg = message.reply_to_message.text
-        async for dialog in client.iter_dialogs():
+        lmao = message.reply_to_message.text
+        async for dialog in USER.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
-                await asyncio.sleep(0.7)
+                #await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
                 
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+            
+        await message.reply_text(f"`Broadcast Finished ` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
